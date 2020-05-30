794e3603653d4fb2a93068a5205c406bd0755837
TaigaMikami <mikamimikami60@gmail.com>
2020-05-30 19:48:59 +0900

    add retrieve article test

--- api/retrieve_articles_test.go
+++ api/retrieve_articles_test.go
@@ -22,3 +22,16 @@ func TestRetrieveArticles(t *testing.T) {
 		t.Errorf("Got wrong number of articles expect: %d, actual: %d\n", perPageNum, len(articles))
 	}
 }
+
+func TestRetrieveArticle(t *testing.T) {
+	id := 150589
+	client := NewClient("")
+	article, err := client.RetrieveArticleById(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if article.Id != id {
+		t.Errorf("Got wrong id expect: %d, actual: %d\n", id, article.Id)
+	}
+}
