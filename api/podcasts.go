842cd300e842989fde4b75fd72d48e59dd12c188
TaigaMikami <mikamimikami60@gmail.com>
2020-05-31 22:37:07 +0900

    fix struct name

--- api/podcasts.go
+++ api/podcasts.go
@@ -1,13 +1,13 @@
 package api
 
-type RetrievePodcast struct {
+type RetrievePodcastOption struct {
 	Page     int
 	PerPage  int
 	Username string
 }
 
 /* PUT /podcast_episodes */
-func (c *Client) RetrievePodcastEpisodes(option *RetrievePodcast) ([]*PodcastEpisode, error) {
+func (c *Client) RetrievePodcastEpisodes(option *RetrievePodcastOption) ([]*PodcastEpisode, error) {
 	var res []*PodcastEpisode
 	err := GetWithQuery("/podcast_episodes", c.ApiKey, option, &res)
 	if err != nil {
