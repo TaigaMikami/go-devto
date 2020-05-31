70a7f5c5d5650d793549320e61c3a559b5707c16
TaigaMikami <mikamimikami60@gmail.com>
2020-05-31 22:38:12 +0900

    fix comment out

--- api/podcasts.go
+++ api/podcasts.go
@@ -6,7 +6,7 @@ type RetrievePodcastOption struct {
 	Username string
 }
 
-/* PUT /podcast_episodes */
+/* GET /podcast_episodes */
 func (c *Client) RetrievePodcastEpisodes(option *RetrievePodcastOption) ([]*PodcastEpisode, error) {
 	var res []*PodcastEpisode
 	err := GetWithQuery("/podcast_episodes", c.ApiKey, option, &res)
