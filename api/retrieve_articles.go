a7eb81be0fd449a51ef67540be571d2a0714afd0
TaigaMikami <mikamimikami60@gmail.com>
2020-05-31 15:42:27 +0900

    Separate article domain

--- api/retrieve_articles.go
+++ api/retrieve_articles.go
@@ -20,55 +20,6 @@ const (
 	StateAll          = "all"
 )
 
-type Article struct {
-	TypeOf               string       `json:"type_of"`
-	Id                   int          `json:"id"`
-	Title                string       `json:"title"`
-	Description          string       `json:"description"`
-	CoverImage           string       `json:"cover_image"`
-	ReadablePublishData  string       `json:"readable_publish_data"`
-	SocialImage          string       `json:"social_image"`
-	Slug                 string       `json:"slug"`
-	Path                 string       `json:"path"`
-	Url                  string       `json:"url"`
-	CanonicalUrl         string       `json:"canonical_url"`
-	CommentsCount        int          `json:"comments_count"`
-	PublicReactionsCount int          `json:"public_reactions_count"`
-	CreatedAt            string       `json:"created_at"`
-	EditedAt             string       `json:"edited_at"`
-	CrosspostedAt        string       `json:"crossposted_at"`
-	PublishedAt          string       `json:"published_at"`
-	LastCommentAt        string       `json:"last_comment_at"`
-	PublishedTimestamp   string       `json:"published_timestamp"`
-	User                 User         `json:"user"`
-	Organization         Organization `json:"organization"`
-	FlareTag             FlareTag     `json:"flare_tag"`
-}
-
-type User struct {
-	Name            string `json:"name"`
-	Username        string `json:"username"`
-	TwitterUsername string `json:"twitter_username"`
-	GithubUsername  string `json:"github_username"`
-	WebsiteUrl      string `json:"website_url"`
-	ProfileImage    string `json:"profile_image"`
-	ProfileImage90  string `json:"profile_image_90"`
-}
-
-type Organization struct {
-	Name           string `json:"name"`
-	Username       string `json:"username"`
-	Slug           string `json:"slug"`
-	ProfileImage   string `json:"profile_image"`
-	ProfileImage90 string `json:"profile_image_90"`
-}
-
-type FlareTag struct {
-	Name         string `json:"name"`
-	BgColorHex   string `json:"bg_color_hex"`
-	TextColorHex string `json:"text_color_hex"`
-}
-
 func (c *Client) RetrieveArticles(option *RetrieveArticlesOption) ([]*Article, error) {
 	var res []*Article
 	err := GetWithQuery("/articles", option, &res)
